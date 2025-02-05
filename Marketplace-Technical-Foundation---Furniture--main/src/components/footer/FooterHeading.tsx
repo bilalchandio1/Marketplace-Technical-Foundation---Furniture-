0d123666d356eaece7fd3459f35092387cf89395
NikkiCyber <161043023+muskanBP@users.noreply.github.com>
2025-02-05 12:33:07 -0800

    Delete Marketplace-Technical-Foundation---Furniture--main directory

--- Marketplace-Technical-Foundation---Furniture--main/src/components/footer/FooterHeading.tsx
+++
@@ -1,9 +0,0 @@
-const FooterHeading = ({ name }: { name: string }) => {
-  return (
-    <h5 className="font-clash text-lg tracking-wide text-white font leading-[19.68px] ">
-      {name}
-    </h5>
-  );
-};
-
-export default FooterHeading;
--- Marketplace-Technical-Foundation---Furniture--main/src/components/footer/FooterLinks.tsx
+++
@@ -1,9 +0,0 @@
-const FooterLinks = ({ name }: { name: string }) => {
-  return (
-    <p className="font-satoshi font-normal leading-[18.9px] text-white hover:underline py-2 ">
-      {name}
-    </p>
-  );
-};
-
-export default FooterLinks;
